882974268db8ac7c152b81108edf815e86445a7b
danielshires <hello@danielshires.com>
2022-09-25 12:15:49 +1000

    Assignment complete

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,23 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+
+class Dancer
+
+    # include Dance
+    # extend MetaDancing
+
+    extend FancyDance::ClassMethods
+    # Include is used to add functionality to our classes via instance methods. 
+    include FancyDannce::InstanceMethods
+    
+    attr_accessor :name
+    
+    def initialize(name)
+      @name = name
+    end
+
+    # extend MetaDancing
+
+end
